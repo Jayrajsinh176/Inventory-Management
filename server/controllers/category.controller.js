9170c61584b2151825768e571db315192ba13c56
Harsh-Prajapati2 <harshprajapati020105@gmail.com>
2026-03-30 11:04:12 +0530

    create category backend

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Category from "../models/category.model";
 import Company from "../models/company.model";
+import e from "express";
 
 
 
@@ -56,5 +57,60 @@ export const getCategory = async (req, res) => {
  */
 
 export const createCategory = async (req, res) => {
+    try{
+        let { name } = req.body;
+        name = name?.trim();
+
+        if(!name){
+            return res.status(400).json({
+                message : "Category Name is required."
+            });
+        }
+
+        const company = await Company.findById(req.user.company);
+        if(!company){
+            return res.status(404).json({
+                message:"Company not found"
+            })
+        }
+        const existingCategory = await Category.findOne({
+            company : req.user.company,
+            name:name,
+        })
+        if(existingCategory){
+            return res.status(400).json({
+                message:"Category already exists for this company"
+            })
+        }
+        const category = await Category.create({
+            company:req.user.company,
+            name:name
+        })
+        return res.status(200).json({
+            message:"Category created Successfully.",
+            category
+        })
+    } catch(error){
+        return handleCategoryError(res,error);
+    }
+}
+
+/** 
+ * @description update category for a company
+ * @route POST /api/category/:id
+ * @access Protected
+ */
+
+export const updateCategory = async (req, res) => {
+    
+}
+
+/** 
+ * @description delete category for a company
+ * @route POST /api/category/:id
+ * @access Protected
+ */
+
+export const deleteCategory = async (req, res) => {
     
 }
